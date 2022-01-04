88cf47ef056f520fba6245dc37793c9918924046
Lebohang Petane <lebopetane@gmail.com>
2022-01-05 01:00:39 +0200

    Unable to take messages

--- saveMessage.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header("Content-Type: application/json; charset=UTF-8");
-    $obj = $_GET["x"];
-
-    //replace the file
-    file_put_contents('https://theindispensablemovie.github.io/messages.json', $obj);
-    echo file_get_contents('https://theindispensablemovie.github.io/messages.json');
-?>
