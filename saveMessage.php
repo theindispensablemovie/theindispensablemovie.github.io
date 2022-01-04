f9a7198a4a5e3f36d4d3d37130b4833743992117
Lebohang Petane <lebopetane@gmail.com>
2022-01-04 18:58:10 +0200

    Fixed php bug with writting to files

--- saveMessage.php
+++ saveMessage.php
@@ -3,5 +3,6 @@
     $obj = $_GET["x"];
 
     //replace the file
-    file_put_contents('messages.json', $obj);
+    file_put_contents('https://theindispensablemovie.github.io/messages.json', $obj);
+    echo file_get_contents('https://theindispensablemovie.github.io/messages.json');
 ?>
