b33bf93a17f431f98663bd26cfb3d04f37812f00
Lebohang Petane <lebopetane@gmail.com>
2022-01-08 18:10:01 +0200

    Just trying all options

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults({
-    static:'./public'
+    static:'./'
 });
 const PORT = process.env.PORT || 5000;
 server.use(middlewares);
